6f11cb9bf0e39c089a7c350c07398b3ad6c833a6
Pankaj Yadav <pankaj.yadav.ece17@itbhu.ac.in>
2019-02-26 14:07:49 +0530

    added

--- review_classifier/settings.py
+++ review_classifier/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
